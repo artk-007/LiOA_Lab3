e7a763c118ac08fe299510e926623250c5549402
artk-007 <70705592+artk-007@users.noreply.github.com>
2020-10-09 00:43:35 +0300

    1

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -161,17 +161,16 @@ void ras(int (&constant)[SIZE][SIZE], int(&Matrix)[SIZE + 1][SIZE + 1]) {
 			}
 		}
 	}
-		for (j = 0; j < SIZE; j++) {
-			if (constant[x][j] = 1)
+		/*for (j = 0; j < SIZE; j++) {
+			if (constant[x][j] == 1)
 				if (j % 2 == 0) {
-					Matrix[x][j] = 1;
-					Matrix[j][x] = 1;
+					Matrix[x][j+1] = 1;
+					Matrix[j+1][x] = 1;
+					Matrix[x + 2][j] = 0;
+					Matrix[j][x + 2] = 0;
 				}
-				else {
-					Matrix[x + 1][j] = 1;
-					Matrix[j][x + 1] = 1;
-				}
-		}
+				
+		}*/
 		printf("Результат расщепления\n");
 	printf("\n");
 	for (i = 0; i < SIZE + 1; i++) {
@@ -221,14 +220,14 @@ int main(){
 	setlocale(LC_ALL, "Rus");
 	rand_Zap(1,G1);
 	print_G(G1);
-	rand_Zap(2,G2);
-	print_G(G2);
+	//rand_Zap(2,G2);
+	//print_G(G2);
 	
-	otj(G1, G3, G4);
+	//otj(G1, G3, G4);
 
-	stig(G1, G3, G4);
-	//ras(G1, G5);
-	obed(G1, G2);
-	perseh(G1, G2);
-	kolc_sum(G1, G2);
+	//stig(G1, G3, G4);
+	ras(G1, G5);
+	//obed(G1, G2);
+	//perseh(G1, G2);
+	//kolc_sum(G1, G2);
 }
